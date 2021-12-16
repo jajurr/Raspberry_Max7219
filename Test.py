42f5aaf80cab42404d41aa2c442d994117c206f7
jajurr <57657641+jajurr@users.noreply.github.com>
2021-12-16 23:13:17 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -24,15 +24,15 @@ def demo(n, block_orientation, rotate, inreverse):
 
     # start demo
     while True:
-    msg = "Frohe"
-    for char in msg:
+        msg = "Frohe"
+        for char in msg:
         show_message(device, char, fill="white", font=proportional(CP437_FONT))
         time.sleep(0.1)
     
-    msg = "Weihnachten"
-    for char in msg:
-        show_message(device, char, fill="white", font=proportional(CP437_FONT))
-        time.sleep(0.1)
+        msg = "Weihnachten"
+        for char in msg:
+            show_message(device, char, fill="white", font=proportional(CP437_FONT))
+            time.sleep(0.1)
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='matrix_demo arguments',
