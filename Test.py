44ab96c024ba24f7f33f5b3a5281ca7a985c8cb7
jajurr <57657641+jajurr@users.noreply.github.com>
2021-12-16 22:47:42 +0100

    Update Test.py
    Erneut überarbeitet

--- Test.py
+++ Test.py
@@ -3,42 +3,49 @@
 # Copyright (c) 2017-18 Richard Hull and contributors
 # See LICENSE.rst for details.
 
+import re
 import time
 import argparse
+
 
 from luma.led_matrix.device import max7219
 from luma.core.interface.serial import spi, noop
 from luma.core.render import canvas
-from luma.core.legacy import text
-from luma.core.legacy.font import proportional, LCD_FONT
+from luma.core.virtual import viewport
+from luma.core.legacy import text, show_message
+from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
 
-
-def demo(w, h, block_orientation, rotate):
+def demo(n, block_orientation, rotate, inreverse):
     # create matrix device
     serial = spi(port=0, device=0, gpio=noop())
-    device = max7219(serial, width=w, height=h, rotate=rotate, block_orientation=block_orientation)
+    device = max7219(serial, cascaded=12 or 1, block_orientation=-90,
+                     rotate=rotate or 0, blocks_arranged_in_reverse_order=inreverse)
     print("Created device")
 
-    with canvas(device) as draw:
-        draw.rectangle(device.bounding_box, outline="white")
-        text(draw, (2, 2), "Frohe", fill="white", font=proportional(LCD_FONT))
-        text(draw, (2, 10), "Weihnachten", fill="white", font=proportional(LCD_FONT))
+    # start demo
+    while True:
+        msg = "Frohe"
+        print(msg)
+        show_message(device, msg, fill="white", font=proportional(CP437_FONT))
+        time.sleep(0.1)
 
-    time.sleep(300)
-
-
+        msg = "Weihnachten"
+        print(msg)
+        show_message(device, msg, fill="white", font=proportional(CP437_FONT))
+        time.sleep(1)
+    
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='matrix_demo arguments',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('--width', type=int, default=8, help='Width')
-    parser.add_argument('--height', type=int, default=8, help='height')
-    parser.add_argument('--block-orientation', type=int, default=-90, choices=[0, 90, -90], help='Corrects block orientation when wired vertically')
-    parser.add_argument('--rotate', type=int, default=0, choices=[0, 1, 2, 3], help='Rotation factor')
+    parser.add_argument('--cascaded', '-n', type=int, default=1, help='Number of cascaded MAX7219 LED matrices')
+    parser.add_argument('--block-orientation', type=int, default=0, choices=[0, 90, -90], help='Corrects block orientation when wired vertically')
+    parser.add_argument('--rotate', type=int, default=0, choices=[0, 1, 2, 3], help='Rotate display 0=0°, 1=90°, 2=180°, 3=270°')
+    parser.add_argument('--reverse-order', type=bool, default=False, help='Set to true if blocks are in reverse order')
 
     args = parser.parse_args()
 
     try:
-        demo(args.width, args.height, args.block_orientation, args.rotate)
+        demo(args.cascaded, args.block_orientation, args.rotate, args.reverse_order)
     except KeyboardInterrupt:
         pass
