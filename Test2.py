680a3061df657974cc8a06c0986e664234481b48
jajurr <57657641+jajurr@users.noreply.github.com>
2022-02-14 17:28:54 +0100

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -10,4 +10,10 @@ GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input 
 
 while True: # Run forever
     if GPIO.input(10) == GPIO.HIGH:
-        print("Button was pushed!")
+        print("Button 10 was pushed!")
+    if GPIO.input(11) == GPIO.HIGH:
+        print("Button 11 was pushed!")
+    if GPIO.input(12) == GPIO.HIGH:
+        print("Button 12 was pushed!")
+    if GPIO.input(13) == GPIO.HIGH:
+        print("Button 13 was pushed!")
