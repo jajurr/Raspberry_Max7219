a34f79e00d1ccea74fe3526ac5003ed515beacc1
jajurr <57657641+jajurr@users.noreply.github.com>
2022-02-14 18:32:02 +0100

    Wartezeit hinzugefügt

--- Snake.py
+++ Snake.py
@@ -8,19 +8,25 @@ import time
 import max7219.led as led
 from random import randint
 
-
-GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-while True: # Run forever
-    if GPIO.input(15) == GPIO.HIGH:
-        print("Button 10 was pushed!")
-    if GPIO.input(11) == GPIO.HIGH:
-        print("Button 11 was pushed!")
-    if GPIO.input(12) == GPIO.HIGH:
-        print("Button 12 was pushed!")
-    if GPIO.input(13) == GPIO.HIGH:
-        print("Button 13 was pushed!")
+taster = [11,12,13]
+matrixe = 1
+matrix = led.matrix(cascaded=matrixe)
+height = 7
+width = (8*matrixe)-1
+
+
+
+
+for i in taster:
+  gpio.setup(i,gpio.IN,pull_up_down=gpio.PUD_UP)
+  gpio.add_event_detect(i, gpio.FALLING, callback=steuerung)
+        
+def steuerung(gpio):
+  global richtung
+  if GPIO.input(11) == GPIO.HIGH:   #rechts
+    richtung = [1,0]
+  elif GPIO.input(12) == GPIO.HIGH: #links
+    richtung = [-1,0]
+        
+        
--- Test2.py
+++ Test2.py
@@ -2,20 +2,22 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2017-18 Richard Hull and contributors
 # See LICENSE.rst for details.
-
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
-GPIO.setwarnings(False) # Ignore warning for now
+import time
+
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+
 GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-while True: # Run forever
-    if GPIO.input(15) == GPIO.HIGH:
-        print("Button 10 was pushed!")
+
+while 1 == 1: # Run forever
     if GPIO.input(11) == GPIO.HIGH:
         print("Button 11 was pushed!")
+        time.sleep(0.5)
     if GPIO.input(12) == GPIO.HIGH:
         print("Button 12 was pushed!")
+        time.sleep(0.5)
     if GPIO.input(13) == GPIO.HIGH:
         print("Button 13 was pushed!")
+        time.sleep(0.5)
