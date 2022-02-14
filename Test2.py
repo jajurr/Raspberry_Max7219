183fad0fba4ad8c875130d5491b822e0fdc42b19
jajurr <57657641+jajurr@users.noreply.github.com>
2022-02-14 19:27:20 +0100

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -12,6 +12,7 @@ GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input 
 GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
+matrixe = 1
 matrix = led.matrix(cascaded=matrixe)
 
 while 1 == 1: # Run forever
