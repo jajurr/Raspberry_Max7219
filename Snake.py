3130f8aa7562c6455f0444b41b6b2682a63ec163
jajurr <57657641+jajurr@users.noreply.github.com>
2022-02-14 19:25:27 +0100

    Additions

--- Snake.py
+++ Snake.py
@@ -1,32 +1,95 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Copyright (c) 2017-18 Richard Hull and contributors
-# See LICENSE.rst for details.
-
 import RPi.GPIO as gpio
 import time
 import max7219.led as led
 from random import randint
 
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-taster = [11,12,13]
+gpio.setmode(gpio.BCM)
+taster = [14,15,18,23]
 matrixe = 1
 matrix = led.matrix(cascaded=matrixe)
 height = 7
-width = (8*matrixe)-1
+width = 7
 
-
-
+def steuerung(gpio):
+  global richtung
+  if(gpio == 14):   #rechts
+    richtung = [1,0]
+  elif(gpio == 15): #oben
+    richtung = [0,-1]
+  elif(gpio == 18): #unten
+     richtung = [0,1]
+  elif(gpio == 23): #links
+    richtung = [-1,0]
 
 for i in taster:
   gpio.setup(i,gpio.IN,pull_up_down=gpio.PUD_UP)
   gpio.add_event_detect(i, gpio.FALLING, callback=steuerung)
-        
-def steuerung(gpio):
-  global richtung
-  if GPIO.input(11) == GPIO.HIGH:   #rechts
-    richtung = [1,0]
-  elif GPIO.input(12) == GPIO.HIGH: #links
-    richtung = [-1,0]
-        
-        
+
+def startSpiel():
+  global snake, richtung, apfel
+  snake = [[randint(2,width-4),randint(3,height-3)]]
+  richtung = [0,0]
+  while richtung == [0,0]:
+    matrix.show_message("READY")
+  neuerApfel()
+
+def neuerApfel():
+  global apfel, snake
+  apfelSnake = False
+  while apfelSnake == False:
+    apfelSnake = True
+    apfel = [randint(0,width),randint(0,height)]
+    for i in snake:
+      if(i == apfel):
+        apfelSnake = False
+  print(apfel)
+
+
+def endOfGame():
+  for i in range(0,2):
+    matrix.clear()
+    for i in range(0,width+1):
+      for j in range(0,height+1):
+        matrix.pixel(i,j,1)
+        time.sleep(0.001)
+    time.sleep(0.01)
+  matrix.show_message("GAME OVER")
+  punkte = len(snake)-1
+  matrix.show_message(str(punkte)+" PUNKTE")
+
+  startSpiel()
+
+startSpiel()
+
+while True:
+  keinePause = False
+  newSnake = [snake[0][0]+richtung[0],
+              snake[0][1]+richtung[1]]
+  for i in snake:
+    if(i == newSnake):
+      endOfGame()
+      pass
+
+  if(newSnake == apfel):
+     neuerApfel()
+     keinePause = True
+  else:
+     snake.pop()
+  snake.insert(0,newSnake)
+
+
+  if(snake[0][0] > width or snake[0][1] > height
+	or snake[0][0] < 0 or snake[0][1] < 0 ):
+    endOfGame()
+    pass
+
+
+  matrix.clear()
+  for i in snake:
+    matrix.pixel(i[0],i[1], 1)
+  matrix.pixel(apfel[0],apfel[1],1)
+  if(keinePause == False):
+    newLength = (len(snake)-2)*0.01
+    time.sleep(0.3-newLength)
+  else:
+    time.sleep(0.3)
--- Test2.py
+++ Test2.py
@@ -3,6 +3,7 @@
 # Copyright (c) 2017-18 Richard Hull and contributors
 # See LICENSE.rst for details.
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+import max7219.led as led
 import time
 
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
@@ -11,13 +12,18 @@ GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input 
 GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
+matrix = led.matrix(cascaded=matrixe)
+
 while 1 == 1: # Run forever
     if GPIO.input(11) == GPIO.HIGH:
         print("Button 11 was pushed!")
+        matrix.pixel([0], [0], 1)
         time.sleep(0.5)
     if GPIO.input(12) == GPIO.HIGH:
         print("Button 12 was pushed!")
+        matrix.pixel([0], [1], 1)
         time.sleep(0.5)
     if GPIO.input(13) == GPIO.HIGH:
         print("Button 13 was pushed!")
+        matrix.pixel([1], [1], 1)
         time.sleep(0.5)
