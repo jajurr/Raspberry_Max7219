58b0f6ee72f3f8eb158ef3e3f547208dc2c78773
jajurr <57657641+jajurr@users.noreply.github.com>
2022-02-14 15:38:39 +0100

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -3,22 +3,12 @@
 # Copyright (c) 2017-18 Richard Hull and contributors
 # See LICENSE.rst for details.
 
-import re
-import time
-import argparse
-import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setup(01, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
-from luma.led_matrix.device import max7219
-from luma.core.interface.serial import spi, noop
-from luma.core.render import canvas
-from luma.core.virtual import viewport
-from luma.core.legacy import text, show_message
-from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
-
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-while(True):
-    if(GPIO.input(1) == True):
-        print("Created device")
+while True: # Run forever
+    if GPIO.input(01) == GPIO.HIGH:
+        print("Button was pushed!")
