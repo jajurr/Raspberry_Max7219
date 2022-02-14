4cf6136b65c5752648bf512fd3b1bbf598c0021d
jajurr <57657641+jajurr@users.noreply.github.com>
2022-02-14 16:25:54 +0100

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -10,5 +10,5 @@ GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
 while True: # Run forever
-    if GPIO.input(12) == GPIO.HIGH:
+    if GPIO.input(12) == True:
         print("Button was pushed!")
