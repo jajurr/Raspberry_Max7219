0b1111c2ad2bb59cbebc5ba0dd2f71df675581c7
jajurr <57657641+jajurr@users.noreply.github.com>
2022-02-14 15:11:40 +0100

    Create Test2.py
    Second Test Script added

---
+++ Test2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2017-18 Richard Hull and contributors
+# See LICENSE.rst for details.
+
+import re
+import time
+import argparse
+import RPi.GPIO as GPIO
+
+
+from luma.led_matrix.device import max7219
+from luma.core.interface.serial import spi, noop
+from luma.core.render import canvas
+from luma.core.virtual import viewport
+from luma.core.legacy import text, show_message
+from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+while(True):
+    if(GPIO.input(11) == True):
+        print("Created device")
