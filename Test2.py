eece99ee58154a71ac2cfbe8f9201af5b1c14789
jajurr <57657641+jajurr@users.noreply.github.com>
2022-02-14 16:46:32 +0100

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -6,8 +6,29 @@
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+
+
+print("Created device")
 
 while (True) : # Run forever
+    if (GPIO.input(11) == True) :
+        print("Button was pushed!")
     if (GPIO.input(12) == True) :
+        print("Button was pushed!")
+    if (GPIO.input(13) == True) :
+        print("Button was pushed!")
+    if (GPIO.input(15) == True) :
+        print("Button was pushed!")
+    if (GPIO.input(16) == True) :
+        print("Button was pushed!")
+    if (GPIO.input(17) == True) :
+        print("Button was pushed!")
+    if (GPIO.input(18) == True) :
         print("Button was pushed!")
