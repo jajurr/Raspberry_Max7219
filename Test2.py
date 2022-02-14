d6019797744dad740495df06c855133ba5699da8
jajurr <57657641+jajurr@users.noreply.github.com>
2022-02-14 15:43:11 +0100

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -7,8 +7,8 @@ import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
 while True: # Run forever
-    if GPIO.input(1) == GPIO.HIGH:
+    if GPIO.input(18) == GPIO.HIGH:
         print("Button was pushed!")
