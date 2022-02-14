d5e8899612f9190f34ac9797ab17f07adf55afd3
jajurr <57657641+jajurr@users.noreply.github.com>
2022-02-14 17:12:36 +0100

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -3,26 +3,11 @@
 # Copyright (c) 2017-18 Richard Hull and contributors
 # See LICENSE.rst for details.
 
-import RPi.GPIO as gpio # Import Raspberry Pi GPIO library
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
-gpio.setmode(gpio.BCM)
-
-print("Created device")
-
-while (True) : # Run forever
-    if (gpio == 18):
-        print("Button was pushed!")
-    if (gpio == 17):
-        print("Button was pushed!")
-    if (gpio == 27):
-        print("Button was pushed!")
-    if (gpio == 23):
-        print("Button was pushed!")
-    if (gpio == 22):
-        print("Button was pushed!")
-    if (gpio == 24):
-        print("Button was pushed!")
-    if (gpio == 25):
-        print("Button was pushed!")
-    if (gpio == 9):
+while True: # Run forever
+    if GPIO.input(10) == GPIO.HIGH:
         print("Button was pushed!")
