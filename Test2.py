0c399a778e8415c47d4b872b504c924f56a442db
jajurr <57657641+jajurr@users.noreply.github.com>
2022-02-14 16:31:34 +0100

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -5,7 +5,6 @@
 
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 
-GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
