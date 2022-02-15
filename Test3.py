3365a5b44db8bd15b58b4c1047bf674fe227584d
jajurr <57657641+jajurr@users.noreply.github.com>
2022-02-15 21:58:47 +0100

    Update Test3.py

--- Test3.py
+++ Test3.py
@@ -1,27 +1,16 @@
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 from MyMax7219 import MyMatrix
 from random import randint
 import time
 
 matrix = MyMatrix()
 
-matrix.showMessage('MOIN')
-
-for i in range(9):
-    matrix.letter(str(9-i))
-    time.sleep(0.2)
-
-matrix.clear()
-time.sleep(0.2)
-
-for i in range(8):
-    for j in range(8):
-        matrix.pixel(j,i,1)
-        time.sleep(0.02)
-    time.sleep(0.1)
-
-for i in range(0,250,20):
-    matrix.brightness(i)
-    time.sleep(0.2)
-
-matrix.clear()
-time.sleep(0.2)
+print("Created device")
+while 1 == 1: # Run forever
+    if GPIO.input(11) == GPIO.HIGH:
+        print("Button 11 was pushed!")
+        time.sleep(0.5)
+        for i in range(8):
+            for j in range(8):
+                matrix.pixel(i,j,randint(0,1))
+                time.sleep(0.0002)
