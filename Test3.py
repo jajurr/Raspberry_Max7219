c8861239177d23ee8e98b9bf8343457c14c1016a
jajurr <57657641+jajurr@users.noreply.github.com>
2022-02-15 19:22:20 +0100

    Update Test3.py

--- Test3.py
+++ Test3.py
@@ -1,18 +1,26 @@
+#!/usr/bin/env python
+
 import time
 from random import randrange
 import max7219.led as led
 
 # create matrix device
-device = led.matrix(cascaded=2)
+device = led.matrix(cascaded=1)
 device.brightness(7)
-
 try:
   while True:
+    # zeilenweise von links nach rechts einschalten
     device.clear()
-    print("Created device")
-    
-    device.pixel(1, 1, 1, redraw=True)
-    time.sleep(1)
+    for y in range(8):
+      for x in range(8):
+        device.pixel(x, y, 1, redraw=True)
+        time.sleep(0.1)
+
+    # und wieder ausknipsen
+    for y in range(8):
+      for x in range(8):
+        device.pixel(x, y, 0, redraw=True)
+        time.sleep(0.1)
 
 except KeyboardInterrupt:
   device.clear()
