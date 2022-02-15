c89818372b5ad178d0dac2966a0d2b09c7be880d
jajurr <57657641+jajurr@users.noreply.github.com>
2022-02-15 22:07:50 +0100

    Update Test3.py

--- Test3.py
+++ Test3.py
@@ -5,6 +5,13 @@ import time
 
 matrix = MyMatrix()
 
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+
+GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+
+
 print("Created device")
 while 1 == 1: # Run forever
     if GPIO.input(11) == GPIO.HIGH:
