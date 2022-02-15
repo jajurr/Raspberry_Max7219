6a1434d13dfcc19d68d7ddba6058ce3a37c3065a
jajurr <57657641+jajurr@users.noreply.github.com>
2022-02-15 19:14:56 +0100

    Update Test3.py

--- Test3.py
+++ Test3.py
@@ -1,9 +1,22 @@
-from luma.core.interface.serial import spi, noop
-from luma.core.render import canvas
-from luma.led_matrix.device import max7219
-from luma.core.legacy.font import proportional, LCD_FONT
+import time
+from random import randrange
+import max7219.led as led
 
-serial = spi(port=0, device=0, gpio=noop(), block_orientation=-90)
-device = max7219(serial, width=32, height=24)
-with canvas(device) as draw:
-    text(draw, (2, 2), ".", fill="white", font=proportional(LCD_FONT))
+# create matrix device
+device = led.matrix(cascaded=2)
+device.brightness(7)
+
+try:
+  while True:
+    device.clear()
+
+    # eine LED durch alle Zeilen laufen lassen
+    for y in range(8):
+      for x in range(8):
+        device.pixel(x, y, 1, redraw=True)
+        time.sleep(0.01)
+        device.pixel(x, y, 0, redraw=True)
+        time.sleep(0.01)
+
+except KeyboardInterrupt:
+  device.clear()
