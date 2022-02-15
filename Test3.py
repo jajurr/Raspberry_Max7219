d086f1140c2e6c70abf8a3f4dc34b41ef16c5025
jajurr <57657641+jajurr@users.noreply.github.com>
2022-02-15 21:47:59 +0100

    Update Test3.py

--- Test3.py
+++ Test3.py
@@ -1,6 +1,27 @@
-from luma.led_matrix.device import max7219
-from luma.core.render import canvas
-...
-device = max7219(...
-with canvas(device) as draw:
-   draw.point([1],[2]), fill="white")
+from MyMax7219 import MyMatrix
+from random import randint
+import time
+
+matrix = MyMatrix()
+
+matrix.showMessage('MOIN')
+
+for i in range(9):
+    matrix.letter(str(9-i))
+    time.sleep(0.2)
+
+matrix.clear()
+time.sleep(0.2)
+
+for i in range(8):
+    for j in range(8):
+        matrix.pixel(j,i,1)
+        time.sleep(0.02)
+    time.sleep(0.1)
+
+for i in range(0,250,20):
+    matrix.brightness(i)
+    time.sleep(0.2)
+
+matrix.clear()
+time.sleep(0.2)
