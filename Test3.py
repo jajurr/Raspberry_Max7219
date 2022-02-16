dccb5e0f4513f7ce3a0bdd23c6455f96adddd92a
jajurr <57657641+jajurr@users.noreply.github.com>
2022-02-16 17:26:38 +0100

    Update Test3.py

--- Test3.py
+++ Test3.py
@@ -5,10 +5,28 @@ import time
 
 matrix = MyMatrix()
 
+intRichtung = 0
+
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 
 def Richtung(gpio):
-	print("Richtung was called!")
+    if GPIO.input(11) == GPIO.HIGH:
+        print("Button 11 was pushed!")
+        intRichtung += 1
+    if GPIO.input(12) == GPIO.HIGH:
+        print("Button 12 was pushed!")
+        intRichtung += 3
+    match intRichtung % 4:
+        case 0:
+            print("Modulo 0")
+        case 1:
+            print("Modulo 1")
+        case 2:
+            print("Modulo 2")
+        case 3:
+            print("Modulo 3")
+        case _:
+	    print("Modulo Error!")
 
 GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 GPIO.add_event_detect(11, GPIO.FALLING, callback=Richtung)
