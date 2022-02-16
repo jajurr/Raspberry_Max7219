980f3dfaad63fb077ed72381449845c6cfe9c952
jajurr <57657641+jajurr@users.noreply.github.com>
2022-02-16 17:56:07 +0100

    Update Test3.py

--- Test3.py
+++ Test3.py
@@ -44,10 +44,4 @@ GPIO.add_event_detect(13, GPIO.FALLING, callback=Richtung)
 	
 print("Created device")
 while 1 == 1: # Run forever
-    if GPIO.input(11) == GPIO.HIGH:
-        print("Button 11 was pushed!")
-        time.sleep(0.5)
-        for i in range(8):
-            for j in range(8):
-                matrix.pixel(i,j,randint(0,1))
-                time.sleep(0.0002)
+   
